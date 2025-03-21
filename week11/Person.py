29846bbd002bdaa50e53f82e6b68f882aa596019
ColinPorter <colin.jm.porter@gmail.com>
2025-03-21 10:22:01 -0400

    added getters and setters to Person.py

---
+++ week11/Person.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, p_name, p_age, p_height):
+        print("Contructing the person object")
+        # __name is a private variable
+        self.__name = p_name
+        self.__age = p_age
+        self.__height = p_height
+        self.public_prop = "I'm public"
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    def __del__(self):
+        print("The garbage collector is auto-destroying the person object")
